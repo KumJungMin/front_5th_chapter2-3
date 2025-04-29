c5274212ecd8ca92512bbe228c286993126fa6b8
KumJungMin <rmawjdals@naver.com>
2025-04-29 20:47:57 +0900

    refactor: 게시물 추가 대화상자를 컴포넌트로 분리하기

--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -32,6 +32,7 @@ import { postDetailDialog } from "../components/postDetailDialog"
 import { updateCommentDialog } from "../components/updateCommentDialog"
 import { addCommentDialog } from "../components/addCommentDialog"
 import { updatePostDialog } from "../components/updatePostDialog"
+import { addPostDialog } from "../components/addPostDialog"
 
 const PostsManager = () => {
   const navigate = useNavigate()
@@ -530,33 +531,7 @@ const PostsManager = () => {
       </CardContent>
 
       {/* 게시물 추가 대화상자 */}
-      <Dialog open={showAddDialog} onOpenChange={setShowAddDialog}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>새 게시물 추가</DialogTitle>
-          </DialogHeader>
-          <div className="space-y-4">
-            <Input
-              placeholder="제목"
-              value={newPost.title}
-              onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
-            />
-            <Textarea
-              rows={30}
-              placeholder="내용"
-              value={newPost.body}
-              onChange={(e) => setNewPost({ ...newPost, body: e.target.value })}
-            />
-            <Input
-              type="number"
-              placeholder="사용자 ID"
-              value={newPost.userId}
-              onChange={(e) => setNewPost({ ...newPost, userId: Number(e.target.value) })}
-            />
-            <Button onClick={addPost}>게시물 추가</Button>
-          </div>
-        </DialogContent>
-      </Dialog>
+      {addPostDialog({ showAddDialog, setShowAddDialog, newPost, setNewPost, addPost })}
 
       {/* 게시물 수정 대화상자 */}
       {updatePostDialog({ showEditDialog, setShowEditDialog, selectedPost, setSelectedPost, updatePost })}
