946266a897d8b926430c78a1b00ee5468f7a97b4
KumJungMin <rmawjdals@naver.com>
2025-05-01 07:54:49 +0900

    feat: user 엔티티의 api 호출 파일 추가하기

---
+++ src/entities/user/api/fetchUsers.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: number
+  username: string
+  image: string
+}
+
+export const fetchUsers = async (): Promise<User[]> => {
+  const res = await fetch("/api/users?limit=0&select=username,image")
+  if (!res.ok) throw new Error("Failed to fetch users")
+  const data = await res.json()
+  return data.users
+}
