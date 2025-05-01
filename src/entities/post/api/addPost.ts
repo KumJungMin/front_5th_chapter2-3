3a11af1a45e7d513d98efba4c3de4c11064f8eb0
KumJungMin <rmawjdals@naver.com>
2025-05-02 01:33:03 +0900

    chore: axiosInstance 호출 방식 사용하기

--- src/entities/post/api/addPost.ts
+++ src/entities/post/api/addPost.ts
@@ -1,11 +1,7 @@
 import type { Post } from "@/entities/post/model/types"
-import axios from "axios"
-
-const API_BASE_URL = import.meta.env.VITE_API_BASE_PATH
+import { axiosInstance } from "@/shared/lib/axios"
 
 export const addPost = async (newPost: Post): Promise<{ post: Post }> => {
-  const res = await axios.post(`${API_BASE_URL}/api/posts/add`, newPost, {
-    headers: { "Content-Type": "application/json" },
-  })
+  const res = await axiosInstance.post(`/posts/add`, newPost)
   return res.data
 }
