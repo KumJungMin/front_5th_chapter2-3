56819fc053ba8e231abf05a6c5a6776d729e15ec
KumJungMin <rmawjdals@naver.com>
2025-05-01 19:25:50 +0900

    chore: useTagsQuery에 타입 지정하기

--- src/features/tags/api/useTagsQuery.ts
+++ src/features/tags/api/useTagsQuery.ts
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query"
 import { fetchTags } from "@/entities/tag/api/fetchTags"
+import type { Tag } from "@/entities/tag/model/types"
 
 export const useTagsQuery = () => {
   const { data = [] } = useQuery({
     queryKey: ["tags"],
     queryFn: fetchTags,
   })
-  return { tags: data }
+  return { tags: data as Tag[] }
 }
