21c1d1075dcf2eb9d142a06c7b83e8685fbbc5b2
KumJungMin <rmawjdals@naver.com>
2025-05-01 22:39:27 +0900

    fix: 댓글 좋아요 기능 에러 수정하기

--- src/features/comment/api/useLikeComment.ts
+++ src/features/comment/api/useLikeComment.ts
@@ -4,7 +4,7 @@ import { likeComment } from "@/entities/comment/api/likeComment"
 export const useLikeComment = () => {
   const qc = useQueryClient()
   return useMutation({
-    mutationFn: likeComment,
+    mutationFn: ({ id, currentLikes }: { id: number; currentLikes: number }) => likeComment({ id, currentLikes }),
     onSuccess: (updated) => {
       qc.invalidateQueries({ queryKey: ["comments", updated.postId] })
     },
