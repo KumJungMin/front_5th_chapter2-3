06d54f451ba872247c2d6c4ff1fd22dea1c72844
KumJungMin <rmawjdals@naver.com>
2025-05-01 22:41:05 +0900

    feat: 댓글 업데이트시 posts 데이터도 갱신하기

--- src/features/posts/model/usePostTableLogic.ts
+++ src/features/posts/model/usePostTableLogic.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import { usePostTableStore } from "./usePostTableStore"
 import { useDialogStore } from "@/features/dialogs/model/useDialogStore"
 import { useQueryNavigate } from "@/shared/model/useQueryNavigate"
@@ -6,21 +7,23 @@ import { useDeletePost } from "@/features/posts/api/useDeletePost"
 import { usePostStore } from "@/features/posts/model/usePostStore"
 import { useUserStore } from "@/features/user/model/useUserStore"
 import { useUserQuery } from "@/features/user/api/useUserQuery"
-import { useEffect, useState } from "react"
+import { useCommentsQuery } from "@/features/comment/api/useCommentsQuery"
+import { useCommentStore } from "@/features/comment/model/useCommentStore"
 
 export const usePostTableLogic = () => {
   const { skip, limit, tag: selectedTag, sortBy, sortOrder, search, set: setFilter } = usePostTableStore()
-  const { posts, deletePost: removeFromStore, setSelectedPost } = usePostStore()
+  const { posts, deletePost: removeFromStore, setSelectedPost, selectedPost, setPosts } = usePostStore()
+  const { setComments } = useCommentStore()
+
   const [userId, setUserId] = useState<string | null>(null)
 
   const { setSelectedUser } = useUserStore()
   const { data: user } = useUserQuery(userId)
   const { toggle } = useDialogStore()
-
   const { updateURL } = useQueryNavigate()
-
   const { isLoading } = usePostsQuery({ skip, limit, tag: selectedTag })
   const { mutate: deletePostMutate } = useDeletePost()
+  const { data: comments } = useCommentsQuery(selectedPost?.id)
 
   useEffect(() => {
     if (user) {
@@ -28,6 +31,12 @@ export const usePostTableLogic = () => {
       toggle("userDetail", true)
     }
   }, [user])
+
+  useEffect(() => {
+    if (comments?.comments && selectedPost?.id) {
+      setComments(selectedPost.id, comments.comments)
+    }
+  }, [comments, selectedPost?.id])
 
   const handleTagClick = (tag: string) => {
     setFilter({ tag })
